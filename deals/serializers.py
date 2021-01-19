d1cbfde642d5c4c4cfbcfc4c8324a69cdc66c283
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-19 20:18:48 +0300

    Adds logic for processing get & post requests for endpoint deals
    Also adds serializers for it

---
+++ deals/serializers.py
@@ -0,0 +1,28 @@
+import collections
+
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Deal, Gem
+
+
+class FileUploadSerializer(serializers.Serializer):
+    csv_file = serializers.FileField(allow_empty_file=False, read_only=True)
+
+
+class DealSerializer(serializers.Serializer):
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        best_buyers_gems = collections.defaultdict(list)
+
+        for gem in Gem.objects.all():
+            gem_buyers = Deal.objects.filter(item=gem).values_list('customer', flat=True).distinct()
+            buyers_intersection = set(gem_buyers).intersection(set([item['customer'] for item in self.instance]))
+            if len(buyers_intersection) > 1:
+                [best_buyers_gems[buyer].append(gem.name) for buyer in buyers_intersection]
+
+        representation['username'] = User.objects.get(id=instance['customer']).username
+        representation['spent_money'] = instance['spent_money']
+        representation['gems'] = best_buyers_gems[instance['customer']]
+        return representation
--- deals/views.py
+++ deals/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+import csv
+import io
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from rest_framework import status
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Deal, Gem
+from .serializers import FileUploadSerializer, DealSerializer
+
+
+class DealsViewSet(ModelViewSet):
+    serializer_class = DealSerializer
+    queryset = Deal.objects.all()
+
+    def get_serializer(self, *args, **kwargs):
+        if self.action == 'create':
+            serializer_class = FileUploadSerializer
+        else:
+            serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+        return serializer_class(*args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        best_buyers = Deal.objects.values('customer').annotate(
+            spent_money=Sum('total')).order_by('-spent_money')[:5]
+        serializer = self.get_serializer(best_buyers, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        if 'file' not in request.data:
+            raise ParseError("Empty content")
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        file = serializer.initial_data['file']
+        decoded_file = file.read().decode()
+        io_string = io.StringIO(decoded_file)
+        reader = csv.reader(io_string)
+
+        fieldnames = []
+        for row in reader:
+            if not fieldnames:
+                fieldnames = row
+                print(fieldnames)
+                continue
+            new_deal, created = Deal.objects.get_or_create(customer=User.objects.get_or_create(username=row[0])[0],
+                                                           item=Gem.objects.get_or_create(name=row[1])[0],
+                                                           total=row[2],
+                                                           quantity=row[3],
+                                                           date=row[4])
+            if created:
+                print(f'{row} has been entered into the database as {new_deal}')
+            else:
+                print(f'{row} already exists as {new_deal}')
+
+        return Response("OK", status=status.HTTP_204_NO_CONTENT)
