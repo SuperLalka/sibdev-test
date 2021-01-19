7aa11d702ee092cd133b87f4316c783118b79f9f
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-19 20:15:28 +0300

    Creates models for processing incoming data

--- deals/models.py
+++ deals/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+DEAL_TOTAL = "Total transaction amount"
+DEAL_QUANTITY = "Total number of gems"
+
+
+class Deal(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_deals')
+    item = models.ForeignKey('Gem', on_delete=models.CASCADE, related_name='gem_deals')
+    total = models.PositiveIntegerField(default=None, help_text=DEAL_TOTAL)
+    quantity = models.PositiveSmallIntegerField(default=None, help_text=DEAL_QUANTITY)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.id} / {self.item} {self.total}"
+
+
+class Gem(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
