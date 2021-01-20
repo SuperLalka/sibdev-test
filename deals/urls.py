7d10c734c8ade334f4170ee10a2583aa6534a133
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-20 17:27:08 +0300

    Removes unnecessary ModelViewSet router

--- deals/urls.py
+++ deals/urls.py
@@ -1,15 +1,10 @@
 from django.conf.urls import url
-from django.urls import include
 
-from rest_framework.routers import DefaultRouter
 from . import views
-
-
-routerAPI = DefaultRouter()
-routerAPI.register(r'deals', views.DealsViewSet, basename='deals')
 
 
 app_name = 'deals'
 urlpatterns = [
-    url(r'^api/', include(routerAPI.urls)),
+    url(r'^deals/', views.DealsViewAPI.as_view(), name='deals'),
+
 ]
