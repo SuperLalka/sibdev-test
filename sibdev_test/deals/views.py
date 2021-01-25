26578a8b1cb6eb80f46feefdbda0b34743776ebd
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-25 17:28:45 +0300

    Adds memoization of the request for a list of the best buyers

--- sibdev_test/deals/views.py
+++ sibdev_test/deals/views.py
@@ -3,8 +3,8 @@ import io
 import logging
 
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from django.db.models import Sum
+from memoize import memoize, delete_memoized
 from rest_framework import status
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
@@ -13,18 +13,23 @@ from rest_framework.views import APIView
 from .models import Deal, Gem
 from .serializers import BestBuyersSerializer
 
-
 logger = logging.getLogger(__name__)
 
 
 class DealsViewAPI(APIView):
 
     def get(self, request):
-        best_buyers = (Deal.objects.values('customer')
-                           .annotate(spent_money=Sum('total'))
-                           .order_by('-spent_money')[:5])
-        serializer = BestBuyersSerializer(best_buyers, many=True)
-        return Response(serializer.data)
+        return Response(self._extract_users_list())
+
+    @staticmethod
+    @memoize(timeout=60 * 15)
+    def _extract_users_list():
+        best_buyers = (
+            Deal.objects.values('customer')
+                .annotate(spent_money=Sum('total'))
+                .order_by('-spent_money')[:5]
+        )
+        return BestBuyersSerializer(best_buyers, many=True).data
 
     def post(self, request):
         if 'file' not in request.data:
@@ -51,5 +56,7 @@ class DealsViewAPI(APIView):
                     logger.info(f'{row} already exists as {new_deal}')
             except LookupError:
                 return Response("Error", status=status.HTTP_400_BAD_REQUEST)
+            finally:
+                delete_memoized(self._extract_users_list)
 
         return Response("OK", status=status.HTTP_204_NO_CONTENT)
